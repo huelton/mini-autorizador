1606c36c8ffec8175f44fcfc322931ccfacd66ab
Huelton <hueltondihon@gmail.com>
2023-01-24 16:04:25 -0300

    first commit

---
+++ src/main/java/com/vr/project/TransationalBenefitsApplication.java
@@ -0,0 +1,13 @@
+package com.vr.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransationalBenefitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransationalBenefitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vr/project/TransationalBenefitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vr.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransationalBenefitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
